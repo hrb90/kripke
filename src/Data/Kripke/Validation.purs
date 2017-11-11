63bdbaf6f05b3e996853ae901b7ca756abea9da1
hrb90 <harrisonrbrown90@gmail.com>
2017-11-10 20:36:05 -0500

    Moar validation

--- src/Data/Kripke/Validation.purs
+++ src/Data/Kripke/Validation.purs
@@ -1,9 +1,10 @@
-module Data.Kripke.Validation (Errors, Validation, validateReflexive) where
+module Data.Kripke.Validation (Errors, Validation, validateReflexive, validateTransitive, validateMonotonic) where
   
 import Prelude
 
+import Data.Array (filter)
 import Data.Foldable (and, elem)
-import Data.Kripke.Kripke
+import Data.Kripke.Kripke (KripkeFrame, Model, Node, Relation)
 import Data.Tuple (Tuple(..))
 import Data.Validation.Semigroup (V, invalid)
 
@@ -16,7 +17,27 @@ vMap err test
   | test = pure unit
   | otherwise = invalid [err]
 
+testAccessibility :: Relation -> Node -> Node -> Boolean
+testAccessibility r w w' = elem (Tuple w w') r
+
 validateReflexive :: KripkeFrame -> Validation
 validateReflexive { worlds, relation } = vMap "The accessibility relation is not reflexive" isReflexive
-  where reflexInRelation node = elem (Tuple node node) relation
+  where reflexInRelation = \n -> testAccessibility relation n n
         isReflexive = and $ map reflexInRelation worlds
+
+validateTransitive :: KripkeFrame -> Validation
+validateTransitive { worlds, relation } = vMap "The accessibility relation is not transitive" isTransitive
+  where accessible = testAccessibility relation
+        isTransitive = and $ do
+          (Tuple x y) <- relation
+          z <- filter (accessible y) worlds
+          pure (accessible x z)
+
+validateMonotonic :: Model -> Validation
+validateMonotonic { frame: { worlds, relation }, valuation } = vMap "The valuation is not monotonic" isMonotonic
+  where accessible = testAccessibility relation
+        testAtom a w = elem (Tuple a w) valuation
+        isMonotonic = and $ do
+          (Tuple atom w) <- valuation
+          w' <- filter (accessible w) worlds
+          pure (testAtom atom w')
