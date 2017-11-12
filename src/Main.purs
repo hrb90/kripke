fc7dcde5e419a864c69b2c3d0a42681c7db3d9b1
hrb90 <harrisonrbrown90@gmail.com>
2017-11-11 22:49:56 -0500

    Sparkle frontend :raised_hands:

--- src/Main.purs
+++ src/Main.purs
@@ -2,8 +2,13 @@ module Main where
 
 import Prelude
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Console (CONSOLE, log)
 
-main :: forall e. Eff (console :: CONSOLE | e) Unit
-main = do
-  log "Hello sailor!"
+import Data.Kripke.Kripke (Model, runEvaluation)
+import Logics.Intuitionistic.Validation (validate)
+
+import Sparkle (sparkle)
+
+readModel :: Model -> Unit
+readModel _ = unit
+
+main = sparkle "model validation" validate
