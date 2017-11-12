a998d7add9cd03f0d08593c7ecf9f79c4884efa0
hrb90 <harrisonrbrown90@gmail.com>
2017-11-11 21:24:27 -0500

    Minor fix

--- src/Logics/Modal/Validation.purs
+++ src/Logics/Modal/Validation.purs
@@ -4,14 +4,14 @@ import Prelude
 
 import Data.Either (Either)
 import Data.Kripke.Kripke (Model)
-import Data.Kripke.Validation ( Validation
-                              , toEither
+import Data.Kripke.Validation ( toEither
                               , validateDomain
                               , validateMonotonic
                               , validateReflexive
                               , validateTransitive )
 
 
+validate :: Model -> Either (Array String) Unit
 validate m@{ frame } = toEither $ (validateDomain m) 
                         *> (validateReflexive frame) 
                         *> (validateTransitive frame) 
