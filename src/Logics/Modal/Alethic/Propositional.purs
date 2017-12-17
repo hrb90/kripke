21eabd1aa12eae0e4f38303d1fc174482341f199
hrb90 <harrisonrbrown90@gmail.com>
2017-12-17 01:12:24 -0500

    Add modal logic parser

--- src/Logics/Modal/Alethic/Propositional.purs
+++ src/Logics/Modal/Alethic/Propositional.purs
@@ -2,9 +2,18 @@ module Logics.Modal.Alethic.Propositional (Expr(..), evaluate) where
 
 import Prelude
 
+import Control.Alt ((<|>))
+import Control.Lazy (fix)
 import Data.Array (filter)
 import Data.Foldable (and)
-import Data.Kripke.Kripke (Atom, Model, Node, testK)
+import Data.Functor (voidRight)
+import Data.Generic (class Generic)
+import Data.Kripke.Kripke (Model, Node, Atom, Evaluation(..), testK)
+import Logics.Intuitionistic.Validation (validate)
+import Parsing.Token (modalToken)
+import Text.Parsing.Parser (Parser)
+
+import Text.Parsing.Parser.Expr (Assoc(..), Operator(..), buildExprParser)
 
 -- A formula of modal propositional logic
 data Expr = Taut | Bottom
@@ -13,6 +22,40 @@ data Expr = Taut | Bottom
             | And Expr Expr
             | Or Expr Expr
             | Implies Expr Expr 
+
+derive instance genericExpr :: Generic Expr
+
+-- Parsing!
+parens :: forall a. Parser String a -> Parser String a
+parens = modalToken.parens
+
+reservedOp :: String -> Parser String Unit
+reservedOp = modalToken.reservedOp
+
+reserved :: String -> Parser String Unit
+reserved = modalToken.reserved
+
+identifier :: Parser String String
+identifier = modalToken.identifier
+
+atomParser :: Parser String Expr
+atomParser = Var <$> identifier 
+            <|> reserved "T" $> Top
+            <|> reserved "F" $> Bottom
+
+term :: Parser String Expr -> Parser String Expr
+term p = parens p <|> atomParser
+
+exprParser :: Parser String Expr
+exprParser = fix allExprs where
+  allExprs p = buildExprParser table (term p)
+  table =
+    [ [ Prefix (reservedOp "!" $> Not)
+      , Prefix (reservedOp "[]" $> Box)
+      , Prefix (reservedOp "<>" $> Diamond) ]
+    , [ Infix (reservedOp "||" $> Or) AssocRight ]
+    , [ Infix (reservedOp "&&" $> And) AssocRight ]
+    , [ Infix (reservedOp "->" $> Implies) AssocRight ] ]
 
 evaluate :: Model -> Node -> Expr -> Boolean
 evaluate _ _ Taut = true
