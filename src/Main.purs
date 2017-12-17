ea23951072158aef98ab69e67d209b0b4f0b144e
hrb90 <harrisonrbrown90@gmail.com>
2017-12-17 15:52:58 -0500

    Factor out purescript-glitter

--- src/Main.purs
+++ src/Main.purs
@@ -7,21 +7,18 @@ import DOM (DOM)
 import Data.Array (singleton)
 import Data.Bifunctor (lmap)
 import Data.Either (Either)
-import Data.Kripke.Kripke (Model, Node, runEvaluation)
+import Data.Kripke.Kripke (Model, Node, Evaluation, runEvaluation)
 import Logics.Intuitionistic.Propositional (exprParser, evaluation)
 import Signal.Channel (CHANNEL)
 import Sparkle (sparkle)
-import TestHelpers (glitter')
+import Glitter (glit)
 
-readModel :: Model -> Unit
-readModel _ = unit
-
-thingToSparkle :: Model -> Node -> String -> Either (Array String) Boolean
-thingToSparkle m n s = do
-      expr <- parse s
+combine :: forall err expr. (String -> Either err expr) -> (Evaluation err expr) -> Model -> Node -> String -> Either err Boolean
+combine parser eval m n s = do
+      expr <- parser s
       evaluate m n expr
-      where parse = glitter' exprParser # map (lmap singleton)
-            evaluate = runEvaluation evaluation
+      where evaluate = runEvaluation eval
 
 main :: forall eff. Eff ( channel :: CHANNEL, dom :: DOM | eff ) Unit
-main = sparkle "model validation" thingToSparkle
+main = sparkle "model validation" $ combine parse evaluation where
+      parse = glit exprParser # map (lmap singleton)
