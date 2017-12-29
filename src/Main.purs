4a9efb7f325745eaaa51104f2b1c0560f1574397
hrb90 <harrisonrbrown90@gmail.com>
2017-12-29 13:54:46 -0500

    Integrate closures

--- src/Main.purs
+++ src/Main.purs
@@ -8,16 +8,19 @@ import Data.Array (singleton)
 import Data.Bifunctor (lmap)
 import Data.Either (Either)
 import Data.Kripke.Kripke (Model, Node, Evaluation, runEvaluation)
+import Data.Kripke.Transformations (reflexiveClosure, transitiveClosure)
 import Logics.Intuitionistic.Propositional (exprParser, evaluation)
 import Signal.Channel (CHANNEL)
 import Sparkle (sparkle)
 import Glitter (glit)
 
 combine :: forall err expr. (String -> Either err expr) -> (Evaluation err expr) -> Model -> Node -> String -> Either err Boolean
-combine parser eval m n s = do
-      expr <- parser s
-      evaluate m n expr
-      where evaluate = runEvaluation eval
+combine parser eval m n s = parser s >>= evaluate (close m) n where 
+      evaluate = runEvaluation eval
+      close { frame, valuation, domain } =
+            { frame: reflexiveClosure $ transitiveClosure $ frame 
+            , valuation
+            , domain }
 
 main :: forall eff. Eff ( channel :: CHANNEL, dom :: DOM | eff ) Unit
 main = sparkle "model validation" $ combine parse evaluation where
