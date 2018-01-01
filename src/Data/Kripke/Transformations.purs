5a1a025700280a2783c9175a06c2774e387a92ab
hrb90 <harrisonrbrown90@gmail.com>
2017-12-31 21:34:41 -0500

    Factor out notIn to be more semantic

--- src/Data/Kripke/Transformations.purs
+++ src/Data/Kripke/Transformations.purs
@@ -1,22 +1,25 @@
-module Data.Kripke.Transformations where
+module Data.Kripke.Transformations (reflexiveClosure, transitiveClosure) where
   
 import Prelude
 
 import Data.Array (filter)
-import Data.Foldable (elem)
+import Data.Foldable (class Foldable, elem)
 import Data.Kripke.Kripke (KripkeFrame, testK)
 import Data.Kripke.Validation (isTransitive)
 import Data.Tuple (Tuple(..))
 
+notIn :: forall f a. Foldable f => Eq a => f a -> a -> Boolean
+notIn xs = not <<< flip elem xs
+
 reflexiveClosure :: KripkeFrame -> KripkeFrame
 reflexiveClosure { worlds, relation } = { worlds, relation: relation <> extra }
-  where extra = filter (not $ flip elem relation) (map (\w -> Tuple w w) worlds)
+  where extra = filter (notIn relation) (map (\w -> Tuple w w) worlds)
 
 transitiveClosure :: KripkeFrame -> KripkeFrame
 transitiveClosure frame@{ worlds, relation }
   | isTransitive frame = frame
   | otherwise = transitiveClosure $ { worlds, relation: relation <> extra }
-      where extra = filter (not $ flip elem relation) $ do
+      where extra = filter (notIn relation) $ do
               (Tuple u v) <- relation
               w <- filter (testK relation v) worlds
               pure $ Tuple u w
